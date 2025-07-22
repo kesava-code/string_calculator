50d9846710b2f5a33dc9d6833da7851b0d0247e6
kesava <mogantikesava@gmail.com>
2025-07-22 20:31:35 +0530

    feat: support custom delimiter

--- lib/src/features/string_calculator/services/string_calculator_service.dart
+++ lib/src/features/string_calculator/services/string_calculator_service.dart
@@ -1,8 +1,16 @@
 class StringCalculatorService {
   int add({required String numberString}) {
     if (numberString.isEmpty) return 0;
-    final delimiter = RegExp(r'[,\n]');
-    return numberString
+    Pattern delimiter = RegExp(r'[,\n]');
+    String stringToProcess = numberString;
+    if (numberString.startsWith("//")) {
+      final stringSeperatorIndex = numberString.indexOf('\n');
+      delimiter = numberString.substring(2, stringSeperatorIndex);
+      stringToProcess = numberString.substring(
+        stringSeperatorIndex + 1,
+      );
+    }
+    return stringToProcess
         .split(delimiter)
         .where((stringNumber) => stringNumber.isNotEmpty)
         .map((stringNumber) => int.parse(stringNumber))
