d666525f23d75cd13eedeeab05f3e0018d63595d
kesava <mogantikesava@gmail.com>
2025-07-22 20:49:47 +0530

    refactor: support custom delimiter with a parser

--- lib/src/features/string_calculator/services/string_calculator_service.dart
+++ lib/src/features/string_calculator/services/string_calculator_service.dart
@@ -1,19 +1,36 @@
+class _ParseResult {
+  final Pattern delimiter;
+  final String numbersToProcess;
+  _ParseResult({required this.delimiter, required this.numbersToProcess});
+}
+
 class StringCalculatorService {
   int add({required String numberString}) {
     if (numberString.isEmpty) return 0;
-    Pattern delimiter = RegExp(r'[,\n]');
-    String stringToProcess = numberString;
-    if (numberString.startsWith("//")) {
-      final stringSeperatorIndex = numberString.indexOf('\n');
-      delimiter = numberString.substring(2, stringSeperatorIndex);
-      stringToProcess = numberString.substring(
-        stringSeperatorIndex + 1,
-      );
-    }
-    return stringToProcess
-        .split(delimiter)
+    final _ParseResult parseResult = _parseInput(stringToParse: numberString);
+    return parseResult.numbersToProcess
+        .split(parseResult.delimiter)
         .where((stringNumber) => stringNumber.isNotEmpty)
         .map((stringNumber) => int.parse(stringNumber))
         .fold(0, (previousValue, element) => previousValue + element);
+  }
+
+  _ParseResult _parseInput({required String stringToParse}) {
+    if (!stringToParse.startsWith("//")) {
+      return _ParseResult(
+        delimiter: RegExp(r'[,\n]'),
+        numbersToProcess: stringToParse,
+      );
+    }
+
+    final stringSeperatorIndex = stringToParse.indexOf('\n');
+    final Pattern delimiter = stringToParse.substring(2, stringSeperatorIndex);
+    final String stringToProcess = stringToParse.substring(
+      stringSeperatorIndex + 1,
+    );
+    return _ParseResult(
+      delimiter: delimiter,
+      numbersToProcess: stringToProcess,
+    );
   }
 }
