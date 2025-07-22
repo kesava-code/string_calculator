42425ac3d89c350303b690dade04b4e45c2c6c43
kesava <mogantikesava@gmail.com>
2025-07-22 19:44:07 +0530

    Feat: Add numbers from a comma-separated string

--- lib/src/features/string_calculator/services/string_calculator_service.dart
+++ lib/src/features/string_calculator/services/string_calculator_service.dart
@@ -1,5 +1,12 @@
 class StringCalculatorService {
   int add({required String numberString}) {
-    return 0;
+    if (numberString.isEmpty) return 0;
+
+    final List<String> stringNumbers = numberString.split(",");
+    int sum = 0;
+    for (final stringNumber in stringNumbers) {
+      sum += int.parse(stringNumber);
+    }
+    return sum;
   }
 }
