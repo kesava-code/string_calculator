04f4d4796dd50b14484cd3e60f3af75492faca5a
kesava <mogantikesava@gmail.com>
2025-07-22 20:06:38 +0530

    feat: Handle both  [ \n ] and [ , ] as separators

--- lib/src/features/string_calculator/services/string_calculator_service.dart
+++ lib/src/features/string_calculator/services/string_calculator_service.dart
@@ -1,9 +1,10 @@
 class StringCalculatorService {
   int add({required String numberString}) {
     if (numberString.isEmpty) return 0;
-
+    final delimiter = RegExp(r'[,\n]');
     return numberString
-        .split(",").where((stringNumber) => stringNumber.isNotEmpty)
+        .split(delimiter)
+        .where((stringNumber) => stringNumber.isNotEmpty)
         .map((stringNumber) => int.parse(stringNumber))
         .fold(0, (previousValue, element) => previousValue + element);
   }
